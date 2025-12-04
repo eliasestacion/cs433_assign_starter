3a80421e8cc43c188b8820875acb13d27b170739
eliasestacion <eliasestacion@gmail.com>
2025-12-03 21:48:34 -0800

    first code changes for prog5

--- assign5/pagetable.cpp
+++ assign5/pagetable.cpp
@@ -1,7 +1,7 @@
 /**
 * Assignment 5: Page replacement algorithms
  * @file pagetable.cpp
- * @author ??? (TODO: your name)
+ * @author Elias Estacion and Meliton Rojas
  * @brief This class represents a traditional pagetable data structure.
  * @version 0.1
  */
@@ -11,10 +11,16 @@
 #include "pagetable.h"
 
 // TODO: Add your implementation of PageTable
-// Constrcutor
+// Constructor
 // TODO: Add your code
-PageTable::PageTable(int num_pages) {
+PageTable::PageTable(int num_pages) : pages(num_pages) {
     
+    // Initialize all page entries as invald pages and unmapped
+    for (int i = 0; i < num_pages; ++i) {
+        pages[i].frame_num = -1; // no frame yet
+        pages[i].valid = false; // not in physical memory
+        pages[i].dirty = false; // clean page
+    }
 }
 
 // Destructor
